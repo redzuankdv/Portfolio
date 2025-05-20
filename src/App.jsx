cd44380c37d1acdd2f8b061e6c9d690cecd553f7
redzuankdv <redzuankdv97@gmail.com>
2025-05-20 09:19:38 +0800

    use browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import { Col, Container, Image, Nav, Navbar, Row } from "react-bootstrap";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function Layout() {
   const [hovered, setHovered] = useState(false);
@@ -117,10 +117,10 @@ function Layout() {
 
 export default function App() {
   return (
-    <HashRouter basename="/Portfolio">
+    <BrowserRouter basename="/Portfolio">
       <Routes>
         <Route path="/" element={<Layout />}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
